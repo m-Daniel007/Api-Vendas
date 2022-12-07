c6e1e7082d8c9b8a97a2b1425a01999de290b3c2
Marcos <marcosdanieldossantos@yahoo.com.br>
2022-12-07 12:34:49 -0300

    Iniciando a criação da entidade Users

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,9 @@
 import productRouter from '@modules/products/routes/ProductsRoutes';
 import { Router } from 'express';
+import morgan from 'morgan';
 
 const router = Router();
-
+router.use(morgan('dev'));
 router.use('/products', productRouter);
 
 export default router;
