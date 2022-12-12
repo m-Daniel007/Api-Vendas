18a3e9ae374756786235ff40bef70cc918cce1ab
Marcos <marcosdanieldossantos@yahoo.com.br>
2022-12-12 16:47:17 -0300

    iniciando criação da entidade UserToken

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export default class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
