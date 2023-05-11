72569c3e8e1da08b13c61311e6984b682f819899
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-05-11 14:32:50 -0300

    configuração  do projeto de uma api de vendas

---
+++ src/shared/http/server.ts
@@ -0,0 +1,35 @@
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import ansi from "ansi-colors";
+import routes from "./routes";
+import AppError from "@shared/errors/AppError";
+import "@shared/typeorm";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+
+app.listen(3333, () => {
+  console.log(
+    ansi.underline.italic.bold.cyanBright("Servidor rodando na porta 3333 🚀")
+  );
+});
